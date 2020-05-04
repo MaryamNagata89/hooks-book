2a018ac6cbf79110f6b4fb3733a9eb3ccf40e39b
永田マリ <nagamari80@gmail.com>
2020-05-04 22:59:10 +0900

    display events implements remove event

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React,{ useReducer, useState } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers';
-
+import Event from './Event';
 const App = () => {
     const [state, dispatch] = useReducer(reducer,[])
  const [title, setTitle] = useState('')
@@ -40,15 +40,17 @@ setBody('')
 <table className="table table-hover">
 
 <thead>
-</thead>
-<tbody>
-</tbody>
 <tr>
 <th>ID</th>
 <th>タイトル</th>
 <th>ボディー</th>
 <th></th>
 </tr>
+</thead>
+<tbody>
+{ state.map((event, index) => (<Event key ={index} event={event} dispatch={dispatch} />))}
+
+</tbody>
 </table>
 	</div>
   );
