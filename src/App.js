11b101e45439528dad6298793c788b8a431746f0
永田マリ <nagamari80@gmail.com>
2020-05-04 16:13:52 +0900

    impliment object

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import React,{ useState } from 'react';
 
-const App = () => {
-const [count, setCount] = useState(0);
+const App = props => {
+const [state,setState] = useState(props)
 
-const increment = () => setCount(count + 1)
-const decrement = () => setCount(count - 1)
-const increment2 = () => setCount(previousCount => previousCount + 1)
-const decrement2 = () => setCount(previousCount => previousCount - 1)
-const reset = () => setCount(0)
-const double = () => setCount(count * 2 )
-const third = () => setCount(previousCount =>{if(count % 3 === 0){
-    return previousCount / 3
-}else {
-    return previousCount 
-}
-                                             })
-  return (
-	<>
-    <div>
-	count: {count}
-    </div>
-<div>    
-<button onClick={increment}>+1</button>
-    <button onClick={decrement}>-1</button>
-</div>
-<div>
-<button onClick={increment2}>+1</button>
-<button onClick={decrement2}>-1</button>
-<button onClick={reset}>0</button>
-<button onClick={double}>x2</button>
-      <button onClick={third}>さんの倍数の時だ割る</button>
-</div>
+const {name, price}=state
 
-	</>
+ return (
+    <>
+	<p>現在の{name}は{price}です。</p>
+<button onClick={()=>setState({...state,price:price + 1})}>+1</button>
+<button onClick={()=>setState({...state,price:price - 1})}>-1</button>
+<button onClick={() => setState(props)}>Reset</button>
+<input value={state.name} onChange={e => setState({...state, name:e.target.value})}/>
+    	</>
   );
 }
-
+App.defaultProps = {
+name:'',
+price:1000
+}
 export default App;
